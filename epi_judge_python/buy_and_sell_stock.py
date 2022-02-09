1f90cf39b30838869f2e01254fcac6d8e3896464
Bruno Brant <b.brant@accenture.com>
2022-02-09 07:37:37 -0300

    Solves 4.1, 5.1 and 5.6.

--- epi_judge_python/buy_and_sell_stock.py
+++ epi_judge_python/buy_and_sell_stock.py
@@ -4,11 +4,34 @@ from test_framework import generic_test
 
 
 def buy_and_sell_stock_once(prices: List[float]) -> float:
-    # TODO - you fill in here.
-    return 0.0
+    profit = 0
+
+    for i in range(len(prices)):
+        for j in range(i + 1, len(prices)):
+            new_profit = prices[j] - prices[i]
+            if new_profit > profit:
+                profit = new_profit
+
+    return profit
+
+
+def test(prices: List[float], expected):
+    actual = buy_and_sell_stock_once(prices)
+    assert actual == expected, f"{prices}, expected {expected}, got {actual}"
+    print(f"Passed {prices} == {expected}.")
 
 
 if __name__ == '__main__':
+    test([], 0)
+    test([1, 2], 1)
+    test([2, 1], 0)
+    test([2, 1, 2], 1)
+    test([1, 5], 4)
+    test([1, 5, 4, 10], 9)
+    test([5, 4, 10, 1], 6)
+
+    #exit()
+
     exit(
         generic_test.generic_test_main('buy_and_sell_stock.py',
                                        'buy_and_sell_stock.tsv',
--- epi_judge_python/dutch_national_flag.py
+++ epi_judge_python/dutch_national_flag.py
@@ -8,13 +8,33 @@ from test_framework.test_utils import enable_executor_hook
 RED, WHITE, BLUE = range(3)
 
 
+def swap(A: List[int], idx_a: int, idx_b: int):
+    b = A[idx_b]
+    A[idx_b] = A[idx_a]
+    A[idx_a] = b
+
+
 def dutch_flag_partition(pivot_index: int, A: List[int]) -> None:
-    # TODO - you fill in here.
-    return
+    less_idx = 0
+    greater_idx = len(A) 
+    pivot = A[pivot_index]
+
+    i = 0
+
+    while i < greater_idx:
+        if A[i] < pivot:
+            swap(A, less_idx, i)
+            less_idx += 1
+            i += 1
+        elif A[i] > pivot:
+            greater_idx -= 1
+            swap(A, greater_idx, i)
+        else:
+            i += 1
 
 
 @enable_executor_hook
-def dutch_flag_partition_wrapper(executor, A, pivot_idx):
+def dutch_flag_partition_wrapper(executor, A: List[int], pivot_idx: int):
     count = [0, 0, 0]
     for x in A:
         count[x] += 1
@@ -40,6 +60,8 @@ def dutch_flag_partition_wrapper(executor, A, pivot_idx):
 
 
 if __name__ == '__main__':
+    # A = [1, 0, 2, 0, 2, 1, 2, 1, 2, 0, 0, 0, 1, 0, 2, 1, 0, 2, 0, 1, 0, 2, 1, 0, 2, 1, 2, 0, 2, 1, 1, 2, 2, 0, 1, 1, 0, 1, 1, 1, 2, 1, 0, 1, 2, 1, 2, 1, 2, 2, 2, 0, 1, 0, 1, 1, 2, 1, 1, 2, 2, 1, 1, 1, 0, 2, 0, 1, 2, 1, 1, 1, 0, 2, 0, 1, 2, 1, 1, 2, 1, 2, 2, 1, 0, 1, 2, 2, 1, 2, 2, 1, 1, 2, 0, 1, 0, 1, 2, 0, 2, 1, 2, 1, 1, 1, 2, 2, 2, 0, 0, 0, 1, 2, 0, 0, 0, 0]
+    # dutch_flag_partition(0,  A)
     exit(
         generic_test.generic_test_main('dutch_national_flag.py',
                                        'dutch_national_flag.tsv',
