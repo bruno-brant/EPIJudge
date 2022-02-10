79ea6fece214d1ca0be2aa06b20a7b364f8dcf75
Bruno Brant <b.brant@accenture.com>
2022-02-10 15:36:38 -0300

    Solves 12.2

--- epi_judge_python/is_anonymous_letter_constructible.py
+++ epi_judge_python/is_anonymous_letter_constructible.py
@@ -3,7 +3,22 @@ from test_framework import generic_test
 
 def is_letter_constructible_from_magazine(letter_text: str,
                                           magazine_text: str) -> bool:
-    # TODO - you fill in here.
+
+    available_letters = {}
+    
+    for char in [chr(char).lower() for char in range(ord('0'), ord('z'))]:
+        available_letters[char] = 0    
+    
+    for char in magazine_text:
+        if char in available_letters:
+            available_letters[char] += 1
+
+    for char in letter_text:
+        if char in available_letters:
+            available_letters[char] -= 1
+            if available_letters[char] < 0:
+                return False
+
     return True
 
 
