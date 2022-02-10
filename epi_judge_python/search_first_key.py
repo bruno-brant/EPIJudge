dcb8ea9aebd1d9e704e442ad72a69e4afab2a8c9
Bruno Brant <b.brant@accenture.com>
2022-02-10 12:34:30 -0300

    Adds performance test for 11.1

--- epi_judge_python/search_first_key.py
+++ epi_judge_python/search_first_key.py
@@ -1,6 +1,8 @@
+from threading import Timer
 from typing import List
 from math import floor
 from test_framework import generic_test
+from test_framework.timed_executor import TimedExecutor
 
 
 def search_first_of_k(A: List[int], k: int) -> int:
@@ -22,7 +24,19 @@ def search_first_of_k(A: List[int], k: int) -> int:
     return -1
 
 
+def test(array, target, expected):
+    executor = TimedExecutor(60)
+    actual = executor.run(lambda: search_first_of_k(array, target))
+    assert actual == expected, f"Expected {expected}, got {actual}"
+
+    print(f"Duration {floor(executor.get_timer().get_microseconds() / 1000)} ms")
+
 if __name__ == '__main__':
+    very_long_sample = [10 for i in range(10_000_000)]
+    print("Begin long test")
+    test(very_long_sample, 10 ,0)
+    print("Finish long test")
+
     exit(
         generic_test.generic_test_main('search_first_key.py',
                                        'search_first_key.tsv',
