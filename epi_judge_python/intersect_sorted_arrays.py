d66549f71a70f8968f36cdd600da2f287c98744c
Bruno Brant <b.brant@accenture.com>
2022-02-10 16:09:33 -0300

    Solves 13.1 (brute force)

--- epi_judge_python/intersect_sorted_arrays.py
+++ epi_judge_python/intersect_sorted_arrays.py
@@ -4,8 +4,27 @@ from test_framework import generic_test
 
 
 def intersect_two_sorted_arrays(A: List[int], B: List[int]) -> List[int]:
-    # TODO - you fill in here.
-    return []
+    intersection = []
+
+    if not A or not B:
+        return intersection
+
+    a = 0
+    b = 0
+
+    while a < len(A) and b < len(B):
+        if A[a] < B[b]:
+            a += 1
+        elif A[a] > B[b]:
+            b += 1
+        else:
+            if not intersection or A[a] != intersection[-1]:
+                intersection.append(A[a])
+            
+            a += 1
+            b += 1
+
+    return intersection
 
 
 if __name__ == '__main__':
