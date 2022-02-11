3f4b03b3da9399a359275030ad0b7092ece47785
Bruno Brant <b.brant@accenture.com>
2022-02-11 17:39:47 -0300

    Solves two_sum (17)

--- epi_judge_python/two_sum.py
+++ epi_judge_python/two_sum.py
@@ -4,8 +4,23 @@ from test_framework import generic_test
 
 
 def has_two_sum(A: List[int], t: int) -> bool:
-    # TODO - you fill in here.
-    return True
+    l, r = 0, len(A) - 1
+
+    while l <= r:
+        if A[l] + A[r] == t:
+            return True
+        if A[l] + A[r] < t:
+            l += 1
+        else: # A[l] + A[r] > t:
+            r -= 1
+
+    return False
+
+# 6
+#
+# 1 2 4 5 8
+# l       r
+#       r
 
 
 if __name__ == '__main__':
