f5ce5b6b2e22b7bb3c5c33a4f8dd5e243e30cf8e
Bruno Brant <b.brant@accenture.com>
2022-02-13 13:45:59 -0300

    Solves 4.7 (brute force)

--- epi_judge_python/power_x_y.py
+++ epi_judge_python/power_x_y.py
@@ -1,11 +1,49 @@
+import sys
 from test_framework import generic_test
 
 
 def power(x: float, y: int) -> float:
-    # TODO - you fill in here.
-    return 0.0
+    if y == 0:
+        return 1
+
+    if y < 0:
+        return 1 / power(x, abs(y))
+
+    result = x
+
+    for _ in range(1, y):
+        result *= x
+
+    return result
+
+
+def test(x, y):
+    expected = pow(x, y)
+    actual = power(x, y)
+
+    assert float_eq(
+        expected, actual), f"{x}^{y}: expected {expected}, got {actual}"
+
+
+def float_eq(a: float, b: float) -> bool:
+    diff = abs(a) - abs(b)
+    return diff < sys.float_info.epsilon
 
 
 if __name__ == '__main__':
+    test(0, 1)
+    test(1, 1)
+    test(2, 2)
+    test(2, 3)
+    test(2, 10)
+    test(3, 2)
+    test(3, 4)
+
+    test(1.5, 2)
+    test(0.5, 2)
+
+    test(0.001, 2)
+    test(0.001, 100)
+
     exit(generic_test.generic_test_main('power_x_y.py', 'power_x_y.tsv',
                                         power))
