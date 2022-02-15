d74881ea5e42a674dbbd1cd0dd37d51f3e23435c
Bruno Brant <b.brant@accenture.com>
2022-02-15 15:56:26 -0300

    Solves 9.4

--- epi_judge_python/lowest_common_ancestor_with_parent.py
+++ epi_judge_python/lowest_common_ancestor_with_parent.py
@@ -1,5 +1,5 @@
 import functools
-from typing import Optional
+from typing import List, Optional
 
 from binary_tree_with_parent_prototype import BinaryTreeNode
 from test_framework import generic_test
@@ -8,10 +8,43 @@ from test_framework.test_failure import TestFailure
 from test_framework.test_utils import enable_executor_hook
 
 
-def lca(node0: BinaryTreeNode,
-        node1: BinaryTreeNode) -> Optional[BinaryTreeNode]:
-    # TODO - you fill in here.
-    return None
+class Path:
+    def __init__(self) -> None:
+        self.path: set[int] = set()
+
+    def __add__(self, node: BinaryTreeNode):
+        self.path.add(id(node))
+        return self
+
+    def __contains__(self, node: BinaryTreeNode):
+        return id(node) in self.path
+
+
+def lca(node0: BinaryTreeNode, node1: BinaryTreeNode) -> Optional[BinaryTreeNode]:
+    if node0 is None or node1 is None:
+        return None
+
+    if node0 is node1:
+        return node0
+
+    path_node0 = Path() + node0
+    path_node1 = Path() + node1
+
+    while True:
+        if node0.parent:
+            node0 = node0.parent
+            path_node0 += node0
+            if node0 in path_node1:
+                return node0
+
+        if node1.parent:
+            node1 = node1.parent
+            path_node1 += node1
+            if node1 in path_node0:
+                return node1
+
+        if not node0.parent and not node1.parent:
+            return None
 
 
 @enable_executor_hook
