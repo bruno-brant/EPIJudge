93250fd7ba8c76d8d408fec3056061441fda7f06
Bruno Brant <b.brant@accenture.com>
2022-02-24 19:31:16 -0300

    Solves 16.2

--- epi_judge_python/levenshtein_distance.py
+++ epi_judge_python/levenshtein_distance.py
@@ -1,10 +1,37 @@
 from test_framework import generic_test
 
 
-def levenshtein_distance(A: str, B: str) -> int:
-    # TODO - you fill in here.
-    return 0
 
+def levenshtein_distance(A: str, B: str) -> int:
+    cache = {}    
+    return levenshtein_distance_impl(A, 0, B, 0, cache)
+
+
+def levenshtein_distance_impl(A: str, a_idx: int, B: str, b_idx: int, cache: dict) -> int:
+    key = (a_idx, b_idx)
+    
+    if key in cache:
+        return cache[key]
+
+    result = None
+    
+    if a_idx >= len(A):
+        result = len(B) - b_idx
+    elif b_idx >= len(B):
+        result =  len(A) - a_idx
+    elif A[a_idx] == B[b_idx]:
+        result = levenshtein_distance_impl(A, a_idx + 1, B, b_idx + 1, cache)
+
+    else:
+        result = 1 + min(
+            levenshtein_distance_impl(A, a_idx, B, b_idx + 1, cache),
+            levenshtein_distance_impl(A, a_idx + 1, B, b_idx, cache),
+            levenshtein_distance_impl(A, a_idx + 1, B, b_idx + 1, cache)
+        )
+    
+    cache[key] = result
+
+    return result
 
 if __name__ == '__main__':
     exit(
