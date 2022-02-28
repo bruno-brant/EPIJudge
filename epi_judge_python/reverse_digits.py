1efe4f2d6c346460abe16495fb33ea29c0e364f4
Bruno Brant <b.brant@accenture.com>
2022-02-28 19:10:26 -0300

    Solves 4.8

--- epi_judge_python/reverse_digits.py
+++ epi_judge_python/reverse_digits.py
@@ -1,15 +1,32 @@
+import sys
+
 from test_framework import generic_test
 
 
 def reverse(x: int) -> int:
-    is_negative =  -1 if x < 0 else 1
-        
-    r =  int(''.join(reversed(str(abs(x)))))
+    if x < 0:
+        is_negative = -1
+        x = abs(x)
+    else:
+        is_negative = 1
 
-    return is_negative * r
+    result = 0
 
+    while x != 0:
+        result = (result * 10) + (x % 10)
+        x /= 10
+        x = int(x)
+
+    return is_negative * result
+
+
+assert reverse(1) == 1
+assert reverse(10) == 1
+assert reverse(100) == 1
+assert reverse(1000) == 1
+assert reverse(12) == 21
 
 if __name__ == '__main__':
-    exit(
+    sys.exit(
         generic_test.generic_test_main('reverse_digits.py',
                                        'reverse_digits.tsv', reverse))
