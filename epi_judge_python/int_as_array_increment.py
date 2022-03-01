0b794b199baaf62d5af9b17cd05b9c9bdafb573a
Bruno Brant <b.brant@accenture.com>
2022-03-01 18:16:13 -0300

    Solves 5.2

--- epi_judge_python/int_as_array_increment.py
+++ epi_judge_python/int_as_array_increment.py
@@ -3,9 +3,27 @@ from typing import List
 from test_framework import generic_test
 
 
+def last(A: list):
+
+    return A[A-1]
+
+
 def plus_one(A: List[int]) -> List[int]:
-    # TODO - you fill in here.
-    return []
+    add = True
+
+    for i in range(len(A) - 1, -1, -1):
+        if A[i] == 9:
+            A[i] = 0
+        else:
+            A[i] += 1
+            add = False
+            break
+
+    if add:
+        A.append(0)
+        A[0] = 1
+
+    return A
 
 
 if __name__ == '__main__':
