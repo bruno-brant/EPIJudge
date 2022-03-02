4ff7b9554a85a87d55469ea73e3a44195fba0915
Bruno Brant <b.brant@accenture.com>
2022-03-02 07:59:16 -0300

    Solves 8.2

--- epi_judge_python/evaluate_rpn.py
+++ epi_judge_python/evaluate_rpn.py
@@ -1,9 +1,44 @@
+from typing import Generic, TypeVar
 from test_framework import generic_test
+
+T = TypeVar("T")
+
+
+class Stack(Generic[T]):
+    def __init__(self) -> None:
+        self._data = []
+
+    def push(self, value: T) -> None:
+        self._data.append(value)
+
+    def pop(self) -> T:
+        return self._data.pop()
 
 
 def evaluate(expression: str) -> int:
-    # TODO - you fill in here.
-    return 0
+    expressions = expression.split(',')
+
+    s = Stack[int]()
+
+    for expression in expressions:
+        if expression == '+':
+            s.push(s.pop() + s.pop())
+        elif expression == '-':
+            operand_2 = s.pop()
+            operand_1 = s.pop()
+
+            s.push(operand_1 - operand_2)
+        elif expression == '*':
+            s.push(s.pop() * s.pop())
+        elif expression == '/':
+            operand_2 = s.pop()
+            operand_1 = s.pop()
+
+            s.push(operand_1 // operand_2)
+        else:
+            s.push(int(expression))
+
+    return s.pop()
 
 
 if __name__ == '__main__':
