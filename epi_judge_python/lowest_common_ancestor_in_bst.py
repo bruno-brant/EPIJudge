c55b1ac9738e264b68b5e28adb5dfb31a92e797c
Bruno Brant <b.brant@accenture.com>
2022-03-06 21:01:41 -0300

    Solves 14.4

--- epi_judge_python/lowest_common_ancestor_in_bst.py
+++ epi_judge_python/lowest_common_ancestor_in_bst.py
@@ -1,4 +1,5 @@
 import functools
+import sys
 from typing import Optional
 
 from bst_node import BstNode
@@ -10,8 +11,19 @@ from test_framework.test_utils import enable_executor_hook
 
 # Input nodes are nonempty and the key at s is less than or equal to that at b.
 def find_lca(tree: BstNode, s: BstNode, b: BstNode) -> Optional[BstNode]:
-    # TODO - you fill in here.
-    return None
+    browser = tree
+
+    while True:
+        if browser is None:
+            return None
+
+        if browser.data in [s.data, b.data]:
+            return browser
+
+        if s.data < browser.data < b.data:
+            return browser
+
+        browser = browser.left if s.data < browser.data else browser.right
 
 
 @enable_executor_hook
@@ -25,7 +37,7 @@ def lca_wrapper(executor, tree, s, b):
 
 
 if __name__ == '__main__':
-    exit(
+    sys.exit(
         generic_test.generic_test_main('lowest_common_ancestor_in_bst.py',
                                        'lowest_common_ancestor_in_bst.tsv',
                                        lca_wrapper))
