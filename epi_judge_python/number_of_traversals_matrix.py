0222267075470b43421716ebd8b4f1566abfa673
Bruno Brant <b.brant@accenture.com>
2022-03-10 08:30:54 -0300

    Solves 16.3

--- epi_judge_python/number_of_traversals_matrix.py
+++ epi_judge_python/number_of_traversals_matrix.py
@@ -1,13 +1,29 @@
+import sys
 from test_framework import generic_test
+
+cache = {}
 
 
 def number_of_ways(n: int, m: int) -> int:
-    # TODO - you fill in here.
-    return 0
+    result = cache.get((n, m), None)
+
+    if result:
+        return result
+
+    if n == 0 or m == 0:
+        return 0
+
+    if n == m == 1:
+        return 1
+
+    result = number_of_ways(n - 1, m) + number_of_ways(n, m - 1)
+    cache[(n, m)] = result
+
+    return result
 
 
 if __name__ == '__main__':
-    exit(
+    sys.exit(
         generic_test.generic_test_main('number_of_traversals_matrix.py',
                                        'number_of_traversals_matrix.tsv',
                                        number_of_ways))
