c07219826eb0ae71fcbc0d7c6e63d8d97091a5c3
Bruno Brant <b.brant@accenture.com>
2022-03-13 19:53:25 -0300

    Solves 8.3

--- epi_judge_python/is_valid_parenthesization.py
+++ epi_judge_python/is_valid_parenthesization.py
@@ -1,13 +1,40 @@
+import sys
 from test_framework import generic_test
 
 
-def is_well_formed(s: str) -> bool:
-    # TODO - you fill in here.
-    return True
+OPEN = "{[("
+CLOSE = "}])"
 
+
+def index_of(c: str):
+    try:
+        return OPEN.index(c)
+    except ValueError:
+        return -1
+    except Exception as e:
+        raise e
+
+
+def is_well_formed(s: str) -> bool:
+    expected_close = []  # stack of close items
+
+    for c in s:
+        idx = index_of(c)
+        if idx >= 0:
+            expected_close.append(CLOSE[idx])
+        elif expected_close and c == expected_close[-1]:
+            expected_close.pop()
+        elif c in CLOSE:
+            return False
+
+    return not expected_close
+
+
+assert not is_well_formed("}{"), "}{ must be false"
+assert not is_well_formed(")("), "}{ must be false"
 
 if __name__ == '__main__':
-    exit(
+    sys.exit(
         generic_test.generic_test_main('is_valid_parenthesization.py',
                                        'is_valid_parenthesization.tsv',
                                        is_well_formed))
--- problem_mapping.js
+++ problem_mapping.js
@@ -910,7 +910,7 @@ problem_mapping = {
                 "total": 78
             },
             "Python: is_valid_parenthesization.py": {
-                "passed": 4,
+                "passed": 78,
                 "total": 78
             }
         },
