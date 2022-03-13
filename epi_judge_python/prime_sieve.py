2082b279540b15037c1aad6af94c6f553db5c0cd
Bruno Brant <b.brant@accenture.com>
2022-03-13 18:00:58 -0300

    Solves 5.9

--- epi_judge_python/prime_sieve.py
+++ epi_judge_python/prime_sieve.py
@@ -1,3 +1,4 @@
+import sys
 from typing import List
 
 from test_framework import generic_test
@@ -5,11 +6,25 @@ from test_framework import generic_test
 
 # Given n, return all primes up to and including n.
 def generate_primes(n: int) -> List[int]:
-    # TODO - you fill in here.
-    return []
+    if n < 2:
+        return []
+
+    sieve = [True] * (n + 1)
+
+    for i in range(2, n + 1):
+        if sieve[i]:
+            for j in range(i * 2, n + 1, i):
+                sieve[j] = False
+        
+    primes = []
+    for i in range(2, len(sieve)):
+        if sieve[i]:
+            primes.append(i)
+
+    return primes
 
 
 if __name__ == '__main__':
-    exit(
+    sys.exit(
         generic_test.generic_test_main('prime_sieve.py', 'prime_sieve.tsv',
                                        generate_primes))
