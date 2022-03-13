2a50f0e389261275c9f5a63e632bbb342a675f80
Bruno Brant <b.brant@accenture.com>
2022-03-13 10:05:22 -0300

    Solves 4.3

--- epi_judge_python/reverse_bits.py
+++ epi_judge_python/reverse_bits.py
@@ -1,12 +1,37 @@
+import sys
 from test_framework import generic_test
+
+lookup = {
+    0b0000: 0b0000,  # 0
+    0b0001: 0b1000,  # 1
+    0b0010: 0b0100,  # 2
+    0b0011: 0b1100,  # 3
+    0b0100: 0b0010,  # 4
+    0b0101: 0b1010,  # 5
+    0b0110: 0b0110,  # 6
+    0b0111: 0b1110,  # 7
+    0b1000: 0b0001,  # 8
+    0b1001: 0b1001,  # 9
+    0b1010: 0b0101,  # 10
+    0b1011: 0b1101,  # 11
+    0b1100: 0b0011,  # 12
+    0b1101: 0b1011,  # 13
+    0b1110: 0b0111,  # 14
+    0b1111: 0b1111,  # 15
+}
 
 
 def reverse_bits(x: int) -> int:
-    # TODO - you fill in here.
-    return 0
+    n = 0
+
+    for starting_bit in range(0, 64, 4):
+        part = (x >> starting_bit) & 0xF
+        n = n | (lookup[part] << (60 - starting_bit))
+
+    return n
 
 
 if __name__ == '__main__':
-    exit(
+    sys.exit(
         generic_test.generic_test_main('reverse_bits.py', 'reverse_bits.tsv',
                                        reverse_bits))
